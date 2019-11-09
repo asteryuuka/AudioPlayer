dc1002511bd749be1ee2eb30c44a2f2554487e14
Yuuka Kaneko <0416yuuka@gmail.com>
2019-11-09 14:22:23 +0900

    Initial Commit

---
+++ AudioMaker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AudioMaker
+//
+//  Created by 兼子友花 on 11/9/19.
+//  Copyright © 2019 兼子友花. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
