de8bfd61264efddb820ecd9c7af7cc8a92604752
Yuuka Kaneko <0416yuuka@gmail.com>
2019-11-16 18:12:13 +0900

    firebaseに音声ファイルをアップロードする

--- AudioMaker/ViewController.swift
+++ AudioMaker/ViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import AVFoundation
+import Firebase
+import FirebaseFirestore
 
 class ViewController: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDelegate {
     
@@ -21,11 +23,17 @@ class ViewController: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDe
     var audioPlayer: AVAudioPlayer!
     var isRecording = false
     var isPlaying = false
+    var ref: DatabaseReference!
     
-
+    // Get a reference to the storage service using the default Firebase App
+    var storage = Storage.storage()
+    
+    
     override func viewDidLoad() {
+    
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+      ref = Database.database().reference()
     }
     
     func getURL() -> URL{
@@ -98,5 +106,31 @@ class ViewController: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDe
 
            }
        }
+    
+    
+    @IBAction func add(_ sender: AnyObject) {
+         // Create a storage reference from our storage service
+        let storageRef = storage.reference()
+        //getURLでとってきてもらったデータをlocalFileに代入
+        let localFile = getURL()
+        //storageRefにm4aのファイルを入れる
+        let audioRef = storageRef.child("audio/new.m4a")
+        // Upload the file to the path "audio/new.m4a"
+        let uploadTask = audioRef.putFile(from: localFile, metadata: nil) { metadata, error in
+          guard let metadata = metadata else {
+            return
+          }
+          // Metadata contains file metadata such as size, content-type.
+          let size = metadata.size
+          // You can also access to download URL after upload.
+          audioRef.downloadURL { (url, error) in
+            guard let downloadURL = url else {
+              return
+            }
+          }
+        }
+        
+     }
+    
     
 }
---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'AudioMaker' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+  
+  pod 'Firebase/Storage'
+  pod 'Firebase/Database'
+  pod 'Firebase/Firestore'
+  # Pods for AudioMaker
+
+  target 'AudioMakerTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'AudioMakerUITests' do
+    # Pods for testing
+  end
+ 
+end
